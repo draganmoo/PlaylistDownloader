97d2211b8190aa0377aab0c3fcc0034a6b72026c
MiniLau <laurent@desausoi.net>
2019-05-30 16:10:23 +0200

    Playlist items page

--- index/views.py
+++ index/views.py
@@ -5,6 +5,7 @@ from google_api.utils import youtube
 from google_api.utils.authentication import is_valid_credentials
 
 
+
 def _is_authenticated(request):
     return 'credentials' in request.session and \
                 is_valid_credentials(request.session['credentials'])
@@ -28,11 +29,10 @@ def api_my_playlists(request):
 
 def api_my_playlist_items(request):
     playlist_id = request.GET.get('playlist_id', None)
-    print(playlist_id)
     if playlist_id and _is_authenticated(request):
         service = youtube.retrieve_authenticated_service(request.session['credentials'])
         playlist_items = youtube.my_playlist_items(service, playlist_id)
-        return JsonResponse({'playlist_items': playlist_items})
+        return JsonResponse({'playlist_id': playlist_id, 'playlist_items': playlist_items})
 
     if playlist_id:
         return JsonResponse({'error': 'User not authenticated'})
